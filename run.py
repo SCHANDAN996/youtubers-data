d2417dd6e65761e957fc4dc5081dee9737648d25
SCHANDAN996 <work.chandansingh@gmail.com>
2025-10-20 00:32:45 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
 
+# Application factory se app instance banayein
 app = create_app()
 
+# Agar yeh file seedhe chalaayi jaaye, to app ko run karein
 if __name__ == '__main__':
-    app.run()
+    # Debug mode production mein False hona chahiye
+    app.run(debug=False)
